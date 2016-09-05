0479a47e98fd89e761312d070b5b4d0b47e5aa05
lesego-reezy <lesegoseitshiro@gmail.com>
2016-09-05 12:00:55 +0200

    Add files via upload

---
+++ Backend/searchEvent.php
@@ -0,0 +1,16 @@
+  function search_event($name){
+      $query="SELECT id,name FROM tbl_events WHERE name LIKE :ename";
+      $query_params=array(':ename'=>'%'.$name.'%');
+      $data=$this->db_connect_get_many($query,$query_params);
+      if($data){
+        $response["success"]=1;
+        $response["data"]=array();
+        foreach ($data as $key ) {
+          array_push($response["data"],$key);
+        }
+      }else{
+        $response["success"]=0;
+        $response["message"]="No match found";
+      }
+      return $response;
+    }
